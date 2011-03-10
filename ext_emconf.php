c3568c6bafe204056e88ad733b135482b28484fd
professorweb <professorweb@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-10 00:51:12 +0000

    Fixed typo in manual and emconf
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tinysource/trunk@44835 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Tiny Source',
-	'description' => 'The tinysource extension is a small extension which compress the output source code of every page. Basicly it removes linebreaks, tabs, comments and double spaces. The setup of tinysource allows to configure which of these strip-methods shoould be used, separately for <head> and <body>.',
+	'description' => 'The tinysource extension is a small extension which compress the output source code of every page. Basicly it removes linebreaks, tabs, comments and double spaces. The setup of tinysource allows to configure which of these strip-methods should be used, separately for <head> and <body>.',
 	'category' => 'fe',
 	'shy' => 0,
 	'version' => '1.0.0',
