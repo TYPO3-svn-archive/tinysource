1c63faa77e511cdd14644bdef5f03aad1688cda5
dennisr <dennisr@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-10-24 15:12:28 +0000

    refs #41530 Changed ext_localconf.php tinysource is usable with nc_staticfilecache now
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tinysource/trunk@67403 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
-$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][] =
+$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][] =
 	'EXT:tinysource/class.tx_tinysource.php:&tx_tinysource->tinysource';
 
 ?>
