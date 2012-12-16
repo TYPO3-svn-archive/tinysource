195257ca5bf1822ea755cc89ab3355bb8d27836f
professorweb <professorweb@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-12-16 21:47:17 +0000

    refs #43907 Fixed bug which appears if no other extension uses the "contentPostProc-all" system hook before tinysource.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tinysource/trunk@68902 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,6 +2,10 @@
 if (!defined('TYPO3_MODE'))
 	die ('Access denied.');
 
+if (!is_array($TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'])) {
+	$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'] = array();
+}
+
 array_unshift($TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'],
 	'EXT:tinysource/class.tx_tinysource.php:&tx_tinysource->tinysource');
 
