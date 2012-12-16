c57c0f083127ca9a980e992738ca35e0c0b4fba0
professorweb <professorweb@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-12-16 21:49:39 +0000

    Raise to version 2.0.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tinysource/trunk@68903 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,21 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "tinysource".
- *
- * Auto generated 06-12-2012 18:09
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
+########################################################################
+# Extension Manager/Repository config file for ext "tinysource".
+#
+# Auto generated 16-12-2012 22:49
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Tiny Source',
 	'description' => 'Compresses the output source code of every page by removing line breaks, tabs, comments and double spaces. Highly configurable. Works with static file cache extensions.',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '2.0.0',
+	'version' => '2.0.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:6:{s:23:"class.tx_tinysource.php";s:4:"e097";s:12:"ext_icon.gif";s:4:"43ce";s:17:"ext_localconf.php";s:4:"aac0";s:14:"ext_tables.php";s:4:"3884";s:14:"doc/manual.sxw";s:4:"befa";s:16:"static/setup.txt";s:4:"e3ba";}',
+	'_md5_values_when_last_written' => 'a:6:{s:23:"class.tx_tinysource.php";s:4:"e097";s:12:"ext_icon.gif";s:4:"43ce";s:17:"ext_localconf.php";s:4:"ebc5";s:14:"ext_tables.php";s:4:"3884";s:14:"doc/manual.sxw";s:4:"befa";s:16:"static/setup.txt";s:4:"e3ba";}',
 	'suggests' => array(
 	),
 );
