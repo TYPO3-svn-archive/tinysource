090e4e703c3b2b986e70d51ab6e9fe7b64d46102
professorweb <professorweb@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-12-21 10:08:46 +0000

    Remove empty line
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tinysource/tags/2_0_1@69002 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,7 +9,6 @@ if (!is_array($TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['conten
 array_unshift($TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'],
 	'EXT:tinysource/class.tx_tinysource.php:&tx_tinysource->tinysource');
 
-
 $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][] =
 	'EXT:tinysource/class.tx_tinysource.php:&tx_tinysource->tinysource';
 
