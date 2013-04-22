d927b746896dee239ebc326674d8ca9b0363aa4e
professorweb <professorweb@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-04-22 10:08:06 +0000

    Raise version to 2.1.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tinysource/trunk@74707 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "tinysource".
  *
- * Auto generated 10-01-2013 11:08
+ * Auto generated 22-04-2013 12:06
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Compresses the output source code of every page by removing line breaks, tabs, comments and double spaces. Highly configurable. Works with static file cache extensions.',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '2.1.1',
+	'version' => '2.1.2',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:6:{s:23:"class.tx_tinysource.php";s:4:"8e6d";s:12:"ext_icon.gif";s:4:"43ce";s:17:"ext_localconf.php";s:4:"929e";s:14:"ext_tables.php";s:4:"3884";s:14:"doc/manual.sxw";s:4:"befa";s:16:"static/setup.txt";s:4:"e3ba";}',
+	'_md5_values_when_last_written' => 'a:6:{s:23:"class.tx_tinysource.php";s:4:"6ac4";s:12:"ext_icon.gif";s:4:"43ce";s:17:"ext_localconf.php";s:4:"929e";s:14:"ext_tables.php";s:4:"3884";s:14:"doc/manual.sxw";s:4:"befa";s:16:"static/setup.txt";s:4:"e3ba";}',
 	'suggests' => array(
 	),
 );
