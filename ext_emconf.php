81762d6efb01e8975b1209d7ddd490852190031f
professorweb <professorweb@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-05-19 08:57:24 +0000

    Update emconf (wrong TYPO3 dependency). Version 3.0.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tinysource/trunk@85328 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,8 +3,6 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "tinysource".
  *
- * Auto generated 09-04-2014 11:46
- *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
  * writing. "version" and "dependencies" must not be touched!
@@ -15,7 +13,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Compresses the output source code of every page by removing line breaks, tabs, comments and double spaces. Highly configurable. Works with static file cache extensions.',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '3.0.0',
+	'version' => '3.0.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -32,14 +30,13 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.5.0-6.1.99',
+			'typo3' => '4.5.0-6.2.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:6:{s:23:"class.tx_tinysource.php";s:4:"4fc9";s:12:"ext_icon.gif";s:4:"43ce";s:17:"ext_localconf.php";s:4:"929e";s:14:"ext_tables.php";s:4:"3884";s:14:"doc/manual.sxw";s:4:"befa";s:16:"static/setup.txt";s:4:"e3ba";}',
 	'suggests' => array(
 	),
 );
